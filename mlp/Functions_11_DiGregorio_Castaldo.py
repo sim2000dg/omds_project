9fd45a581b7fde392344ac7aa4856f511bb81c3f
sim2000dg <sim2000dg@gmail.com>
2023-06-22 16:54:28 +0200

    *Additional work for backpropagation *Data import fixed

--- mlp/Functions_11_DiGregorio_Castaldo.py
+++ mlp/Functions_11_DiGregorio_Castaldo.py
@@ -1,9 +1,9 @@
 import numpy as np
-from abc import abstractmethod
 from typing import Self
+# from .data_import import csv_import
+import math
 
-
-# Credits to Prof. Galasso's slides for the matrix/tensor approach to backpropagation
+# Credits to Prof. Galasso's slides for guidelines for efficient backpropagation
 
 
 class Linear:
@@ -22,10 +22,11 @@ class Linear:
         self.store = None  # Array used just to avoid useless data copying and array allocation
         self.out = None
         self.tensor_grad = None
+        self.gradient_bias = None
         self.downstream = None
         self.initialized = False
 
-    def _forward(self, input_array: np.array):
+    def forward(self, input_array: np.array):
         """
         Forward pass for Linear layer
         :param input_array: A (batch_size x in_shape) array
@@ -39,7 +40,7 @@ class Linear:
         np.add(self.store, self.bias, out=self.out)  # Adding bias
         return self.out  # Return output of forward pass
 
-    def _backprop(self, upstream_gradient):
+    def backprop(self, upstream_gradient):
         """Returns the downstream tensor gradient for this layer, considering the whole
         set of observations in the forward pass
         :return: The downstream gradient
@@ -47,15 +48,20 @@ class Linear:
         # Save avg. gradient w.r.t. current weights
         np.dot(self.back_store.T, upstream_gradient, out=self.tensor_grad)
         np.mean(self.tensor_grad, axis=0, out=self.gradient)
+        np.add(self.gradient, self.weights, out=self.gradient)
+        np.add(self.gradient, self.weights, out=self.gradient)
+
+        self.gradient_bias = upstream_gradient
 
         # Return downstream gradient
         return np.dot(upstream_gradient, np.transpose(self.weights), out=self.downstream)
 
-    def _model_setup(self, batch_size, in_shape) -> Self:
+    def model_setup(self, batch_size: int, in_shape: int) -> Self:
         """
         Method called when connecting this module to a Model object
         :param batch_size: The batch size for the model
-        :return:
+        :param in_shape: Input shape for the layer
+        :return: The object itself
         """
         self.batch_size = batch_size
         self.in_shape = in_shape
@@ -70,11 +76,15 @@ class Linear:
 
         return self
 
+    def __call__(self, input):
+        return self.forward(input)
+
 
 class HyperTangent:
     """
     A class representing a Hyperbolic Tangent activation, applied element-wise.
     """
+
     def __init__(self, sigma):
         """
         Initialization of Hyperbolic Tangent Activation object
@@ -91,7 +101,7 @@ class HyperTangent:
         self.downstream = None
         self.initialized = False
 
-    def _forward(self, input_array):
+    def forward(self, input_array):
         if not self.initialized:
             raise AttributeError('Your layer is not inside a model, and therefore not initialized')
         self.back_store = input_array
@@ -103,7 +113,7 @@ class HyperTangent:
 
         return self.out
 
-    def _backprop(self, upstream_gradient):
+    def backprop(self, upstream_gradient):
         """
         Returns the gradient for this layer, considering
         the whole set of observations in the forward pass
@@ -113,13 +123,15 @@ class HyperTangent:
         np.square(self.denom, out=self.downstream)
         # Save 4*sigma*exp(2sigma*x)
         np.subtract(self.denom, 1, out=self.denom)
-        np.multiply(4*self.sigma, self.denom, out=self.denom)
+        np.multiply(4 * self.sigma, self.denom, out=self.denom)
         # Save and return the ratio
         np.divide(self.denom, self.downstream, out=self.downstream)
+        # Compute actual downstream gradient
+        np.multiply(self.downstream, upstream_gradient, out=self.downstream)
 
         return self.downstream
 
-    def _model_setup(self, batch_size, in_shape) -> Self:
+    def model_setup(self, batch_size, in_shape) -> Self:
         """
         Method called when connecting this module to a Model object
         :param batch_size: The batch size for the model
@@ -133,28 +145,20 @@ class HyperTangent:
         self.initialized = True
         return self
 
+    def __call__(self, input):
+        return self.forward(input)
+
 
 class Model:
     """
-    A class representing the whole model
+    A class representing the whole MLP model
     """
 
-    def __init__(self):
-        self.layers = list()
-
-    def clean_back(self):
-        for layer in self.layers:
-            del layer.gradie
-
-    def evaluate_loss(self, train_data: np.array, labels: np.array, current_params: np.array) -> Self:
-        """
-        Method evaluating the L2-penalized loss for the training data
-        :param train_data: The training data, as a NumPy array
-        :param labels: The response data, as a 1-D NumPy array
-        :param current_params: The parameters of the network in a 1-D NumPy array
-        :return: The value of the loss
-        """
-        return self
+    def __init__(self, batch_size, input_shape, rho):
+        self.current_out_shape = input_shape
+        self.batch_size = batch_size
+        self.layers: list[Linear | HyperTangent, ...] = list()
+        self.rho: float = rho  # L2 regularization
 
     def add(self, layer: Linear | HyperTangent) -> Self:
         """
@@ -162,12 +166,62 @@ class Model:
         :param layer: The layer object, initialized
         :return: The object itself
         """
+        self.layers.append(layer.model_setup(batch_size=self.batch_size,
+                                             in_shape=self.current_out_shape))
+        return self
 
-    def backprop(self, *args, **kwargs) -> np.array:
+    def backprop(self, upstream_gradient) -> np.array:
         """
         Method starting the backpropagation chain in order to get the overall gradient out
         :returns: The n-D gradient array, where n is the number of parameters in the network
         """
+        gradient_list = list()
+        for layer in reversed(self.layers):
+            upstream_gradient = layer.backprop(upstream_gradient)
+            if isinstance(layer, Linear):
+                local_gradient = layer.gradient
+                gradient_bias = layer.gradient_bias
+                gradient_list.extend([local_gradient, gradient_bias])
+
+        gradient_list = [np.reshape(x, -1) for x in reversed(gradient_list)]
+        gradient = np.concatenate(gradient_list)
+
+        return gradient
+
+    def evaluate_loss(self, train_data: np.array, labels: np.array, current_params: np.array) -> tuple[float, np.array]:
+        """
+        Method evaluating the L2-penalized loss for the training data. It returns the value of the loss and the gradient
+        :param train_data: The training data, as a NumPy array
+        :param labels: The response data, as a 1-D NumPy array
+        :param current_params: The parameters of the network in a 1-D NumPy array
+        :return: A tuple with the value of the loss and the gradient vector
+        """
+        for layer in self.layers:
+            pos = 0
+            if isinstance(layer, Linear):
+                slice_dim_bias = layer.bias.shape
+                slice_dim_weights = math.prod(layer.weights.shape)
+                layer.bias[:] = current_params[pos: pos+slice_dim_bias]
+                pos += slice_dim_bias
+                layer.weights.flat[:] = current_params[pos: pos+slice_dim_weights]
+                pos += slice_dim_weights
+
+        out = train_data
+        reg = 0
+        for layer in self.layers:
+            out = layer(out)  # Computing the forward pass we are
+            # also storing information necessary for the backward pass
+            reg += np.linalg.norm(layer.weights)
+
+        out = 1 / (1 + np.exp(out))  # To be upgraded if we want
+        cross_entropy = np.mean(labels * np.log(out) + 1 - labels * np.log(1 - out)) + self.rho * reg
+
+        downstream_grad = -labels / out + (1 - labels) / (1 - out)  # Cross-entropy gradient
+        # Downstream grad for the rest of the backprop, exploiting the nice analytical shape of the sigmoid derivative
+        downstream_grad = (out * (1-out))*downstream_grad
+        gradient = self.backprop(downstream_grad)
+
+        return cross_entropy, gradient
 
     def evaluate(self, test_data: np.array) -> np.array:
         """
@@ -175,3 +229,14 @@ class Model:
         :params test_data: The array (compatible with the initialized and trained model) containing the test data
         :returns: The 1-D array of predictions
         """
+        pass
+
+
+if __name__ == '__main__':
+    from data_import import csv_import
+
+    train_data = csv_import(['S', 'M'], '../data.txt')
+    model = Model(64, 16)
+    model.add(Linear(2))
+    model.add(HyperTangent(0.5))
+    model.evaluate_loss(train_data[:, :-1], train_data[:, -1])
--- mlp/data_import.py
+++ mlp/data_import.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def csv_import(targets: list[str, ...], path: str) -> tuple[list[str, ...], np.array]:
@@ -10,11 +11,18 @@ def csv_import(targets: list[str, ...], path: str) -> tuple[list[str, ...], np.a
      The second is a NumPy array with the standardized features and the relevant targets encoded in the last column.
     """
 
-    out_array = np.genfromtxt(path, delimiter=',')  # Read CSV
-    out_array = out_array[np.isin(out_array[:, 0], targets)]  # Filter out observations related to rel. variables
-    ord_targets, out_array[:, -1] = np.unique(out_array[:, 0], return_inverse=True)  # Encode target
+    out_array = pd.read_csv(path)  # Read CSV
+    out_array = out_array.loc[out_array['Y'].isin(targets)]  # Filter out observations related to rel. variables
+    ord_targets, out_array.iloc[:, 0] = np.unique(out_array.iloc[:, 0], return_inverse=True)  # Encode target
+    out_array = out_array.to_numpy()
     out_array[:, [0, -1]] = out_array[:, [-1, 0]]  # Swap columns (target last)
 
     out_array[:, :-1] = (out_array[:, :-1]-np.mean(out_array[:, :-1], axis=0))/np.std(out_array[:, :-1])  # standardize
 
     return list(ord_targets), out_array
+
+
+if __name__ == '__main__':
+    data = csv_import(['S', 'M'], '../data.txt')
+
+
