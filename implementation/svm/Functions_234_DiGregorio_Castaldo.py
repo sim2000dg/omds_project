dcc10a8dcb8dd9b2b1ecf2e5531f573f4fbfc28f
sim2000dg <sim2000dg@gmail.com>
2023-07-02 23:00:52 +0200

    * First serious version of SMO

--- implementation/svm/Functions_234_DiGregorio_Castaldo.py
+++ implementation/svm/Functions_234_DiGregorio_Castaldo.py
@@ -1,8 +1,10 @@
 import numpy as np
-from sklearn.metrics import pairwise_distances
+from sklearn.metrics.pairwise import rbf_kernel
 from cvxopt.solvers import qp
 from cvxopt import matrix, spmatrix
 from typing import Optional, Self
+from sympy import solveset, S, Interval
+from sympy.abc import x
 
 
 class GaussianSVM:
@@ -15,8 +17,8 @@ class GaussianSVM:
 
     def cvxopt_fit(self, train_data: np.ndarray, labels: np.ndarray) -> Self:
         gram = matrix(
-            pairwise_distances(
-                train_data, metric=self.gaussian_kernel, gamma=self.gamma
+            rbf_kernel(
+                train_data, gamma=self.gamma
             ),
             tc="d",
         )
@@ -43,44 +45,45 @@ class GaussianSVM:
         return dual_sol
 
     def smo_fit(
-        self, train_data: np.ndarray, labels: np.ndarray, tol: float, max_iter: int
+        self, train_data: np.ndarray, labels: np.ndarray, tol: float, max_iter: int, seed: int
     ):
+        fit_generator = np.random.default_rng(seed)
         current_gradient = np.full(
             fill_value=-1, shape=len(train_data), dtype=np.float64
         )
         current_check = np.zeros_like(current_gradient)
         dual_vars = np.zeros_like(current_gradient)
         grad_update_1 = np.zeros_like(current_gradient)
-        grad_update_2 = np.zeros_like(current_check)
+        grad_update_2 = np.zeros_like(current_gradient)
         set_r = np.full(fill_value=True, shape=len(train_data), dtype=bool)
         set_s = np.copy(set_r)
-        set_r[labels > 0] = True
-        set_s[labels < 0] = True
-        gram = pairwise_distances(
-            train_data, metric=self.gaussian_kernel, gamma=self.gamma
+        gram = rbf_kernel(
+            train_data, gamma=self.gamma
         )
         it = 0
 
         while True:
+            if np.sum(set_r) * np.sum(set_s) == 0:
+                break
             np.multiply(current_gradient, labels, out=current_check)
             np.multiply(current_check, -1, out=current_check)
 
-            max_set_r = np.where(
-                set_r & (current_check == np.max(current_check[set_r]))
+            r_pick = fit_generator.choice(
+                np.where(set_r & (np.isclose(current_check, np.max(current_check[set_r]))))[0],
+                size=1,
             )[0]
-            min_set_s = np.where(
-                set_s & (current_check == np.min(current_check[set_s]))
+            s_pick = fit_generator.choice(
+                np.where(set_s & (np.isclose(current_check, np.min(current_check[set_s]))))[0],
+                size=1,
             )[0]
-            r_pick = max_set_r[0]
-            s_pick = min_set_s[0] if min_set_s[0] != r_pick else min_set_s[1]
 
             if current_check[r_pick] - current_check[s_pick] < tol:
                 break
 
-            sum_pair = -(
-                np.dot(labels, dual_vars)
-                - np.dot(labels[[r_pick, s_pick]], dual_vars[[r_pick, s_pick]])
-            )
+            pred_r = dual_vars[r_pick]
+            pred_s = dual_vars[s_pick]
+
+            sum_pair = - (np.dot(labels, dual_vars)-np.dot(dual_vars[[r_pick, s_pick]], labels[[r_pick, s_pick]]))
             rel_gram = gram[np.ix_([r_pick, s_pick], [r_pick, s_pick])]
             quad_term = (
                 (1 / 2) * rel_gram[0, 0]
@@ -102,30 +105,42 @@ class GaussianSVM:
                 + labels[r_pick] * labels[s_pick]
             )
 
-            pred_r = dual_vars[r_pick]
-            pred_s = dual_vars[s_pick]
-
-            prop = None
-            if quad_term > 0:
-                prop = -lin_term / (2 * quad_term)
-            if prop is None or ((prop < 0) | (prop > self.inv_reg)):
+            prop = -lin_term / (2 * quad_term)
+            if (prop < 0) or (prop > self.inv_reg):
                 prop = (
                     0
                     if 0
                     == min(0, (self.inv_reg ** 2) * quad_term + self.inv_reg * lin_term)
                     else self.inv_reg
                 )
+
             dual_vars[r_pick] = prop
             dual_vars[s_pick] = labels[s_pick] * (
                 -dual_vars[r_pick] * labels[r_pick] + sum_pair
             )
 
+            if dual_vars[s_pick] < 0:
+                if labels[r_pick]*labels[s_pick] > 0:
+                    lower = sum_pair-self.inv_reg
+                    upper = sum_pair
+                else:
+                    lower = sum_pair if labels[r_pick] > 0 else -sum_pair
+                    upper = self.inv_reg+sum_pair if labels[r_pick] > 0 else self.inv_reg-sum_pair
+
+                admissible = np.array([max(0, lower), min(self.inv_reg, upper)])
+                dual_vars[r_pick] = admissible[np.argmin((admissible ** 2)*quad_term + self.inv_reg * lin_term)]
+                dual_vars[s_pick] = labels[s_pick] * (
+                        -dual_vars[r_pick] * labels[r_pick] + sum_pair
+                )
+
             for var in [s_pick, r_pick]:
-                if (dual_vars[var] > 0) & (dual_vars[var] < self.inv_reg):
+                if abs(dual_vars[var]) < 1e-5:
+                    dual_vars[var] = 0
+                if (dual_vars[var] > 0) and (dual_vars[var] < self.inv_reg):
                     set_r[var] = True
                     set_s[var] = True
-                elif ((dual_vars[var] == 0) & (labels[var] > 0)) | (
-                    (dual_vars[var] == self.inv_reg) & (labels[var] < 0)
+                elif ((dual_vars[var] == 0) and (labels[var] > 0)) or (
+                    (dual_vars[var] == self.inv_reg) and (labels[var] < 0)
                 ):
                     set_r[var] = True
                     set_s[var] = False
@@ -144,10 +159,6 @@ class GaussianSVM:
 
         return dual_vars
 
-    @staticmethod
-    def gaussian_kernel(x, y, gamma):
-        return np.exp(-gamma * np.linalg.norm(x - y))
-
 
 if __name__ == "__main__":
     from implementation.data_import import csv_import
@@ -156,6 +167,7 @@ if __name__ == "__main__":
     labels, train_data = csv_import(["S", "M"], "../../data.txt", dtype=np.float64)
     mask = train_data[:, -1] == 0
     train_data[mask, -1] = -1
-    svm = GaussianSVM(gamma=0.1, inv_reg=0.1)
-    dual_sol = svm.smo_fit(train_data[:, :-1], train_data[:, -1], 1e-5, 1e5)
+    svm = GaussianSVM(gamma=0.1, inv_reg=0.01)
+    dual_sol = svm.smo_fit(train_data[:, :-1], train_data[:, -1], 1e-3, 1e5, 1236)
+    dual_sol_cvxopt = svm.cvxopt_fit(train_data[:, :-1], train_data[:, -1])
     print(dual_sol)
