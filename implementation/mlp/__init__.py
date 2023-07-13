3e831baf0658181b450ae248546fc7e93b190568
Mattia Castaldo <castaldomatty@gmail.com>
2023-07-13 17:55:28 +0200

    change __init__.py mlp

--- implementation/mlp/__init__.py
+++ implementation/mlp/__init__.py
@@ -1,2 +1,2 @@
-from .Functions_11_DiGregorio_Castaldo import Model, Linear, HyperTangent
+from .Functions_11_DiGregorio_Castaldo import Model, Linear, HyperTangent, Adam_sciPy
 from .Functions_12_DiGregorio_Castaldo import RBF
